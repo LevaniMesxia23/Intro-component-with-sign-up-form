e256ada44899dc41b7aab24c3943cde9890c4b70
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-03-18 20:30:12 +0400

    added from

--- src/components/inputs.jsx
+++ src/components/inputs.jsx
@@ -1,55 +1,77 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
+import React, { useState } from "react";
+import styled from "styled-components";
 
 export default function Inputs() {
   const [errors, setErrors] = useState({
     name: false,
     lastName: false,
     email: false,
-    password: false
-  })
-    const [values, setValues] = useState({
-      name: "",
-      lastName: "",
-      email: "",
-      password: ""
-    })
+    password: false,
+  });
+  const [values, setValues] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
 
-    function handleInput(event){
-      const newObj = {...values, [event.target.name] : [event.target.value]}
-      setValues(newObj)
+  function handleInput(event) {
+    const newObj = { ...values, [event.target.name]: [event.target.value] };
+    setValues(newObj);
   }
 
-  function validation(){
-
+  function validation(e) {
+    e.preventDefault();
+    if (!values.name) {
+      console.log(values);
+    }
   }
   return (
     <Box>
-      <input
-      type="text"
-      id="name" 
-      placeholder='First Name'/>
-      
-      <input
-      type="text"
-      id="last-name" 
-      placeholder='Last Name'/>
+      <form onSubmit={validation}>
+        <div>
+          <input
+            name="name"
+            onChange={handleInput}
+            type="text"
+            id="name"
+            placeholder="First Name"
+          />
+        </div>
 
-      <input
-      type="email"
-      id="email" 
-      placeholder='Email Adress'/>
+        <input
+          name="lastName"
+          onChange={handleInput}
+          type="text"
+          id="last-name"
+          placeholder="Last Name"
+        />
 
-      <input
-      type="password"
-      id="password" 
-      placeholder='Password'/>
+        <input
+          name="email"
+          onChange={handleInput}
+          type="email"
+          id="email"
+          placeholder="Email Adress"
+        />
 
-      <button onClick={() => validation()}>CLAIM YOUR FREE TRIAL</button>
+        <input
+          name="password"
+          onChange={handleInput}
+          type="password"
+          id="password"
+          placeholder="Password"
+        />
 
-      <div>By clicking the button, you are agreeing to our <span>Terms and Services</span></div>
+        <button>CLAIM YOUR FREE TRIAL</button>
+
+        <div>
+          By clicking the button, you are agreeing to our{" "}
+          <span>Terms and Services</span>
+        </div>
+      </form>
     </Box>
-  )
+  );
 }
 
 const Box = styled.div`
@@ -57,7 +79,7 @@ const Box = styled.div`
   height: 29.625rem;
   flex-shrink: 0;
   border-radius: 0.625rem;
-  background: #FFF;
+  background: #fff;
   box-shadow: 0px 8px 0px 0px rgba(0, 0, 0, 0.15);
   display: flex;
   flex-direction: column;
@@ -65,16 +87,16 @@ const Box = styled.div`
   align-items: center;
   padding: 2.5rem;
 
-  input{
+  input {
     width: 28.75rem;
     height: 3.5rem;
     flex-shrink: 0;
     margin-bottom: 1.25rem;
     border-radius: 0.3125rem;
-    border: 1px solid #5E54A4;
-    background: #FFF;
+    border: 1px solid #5e54a4;
+    background: #fff;
 
-    color: #3D3B48;
+    color: #3d3b48;
     font-family: Poppins;
     font-size: 0.875rem;
     font-style: normal;
@@ -85,26 +107,26 @@ const Box = styled.div`
     padding-left: 1.21rem;
   }
 
-  button{
+  button {
     width: 28.75rem;
     height: 3.5rem;
     flex-shrink: 0;
     border-radius: 0.3125rem;
-    background: #38CC8B;
+    background: #38cc8b;
     box-shadow: 0px -4px 0px 0px rgba(0, 0, 0, 0.09) inset;
     border: none;
-    color: #FFF;
+    color: #fff;
     font-family: Poppins;
     font-size: 0.9375rem;
     font-style: normal;
     font-weight: 600;
-    line-height: 1.625rem; 
+    line-height: 1.625rem;
     letter-spacing: 0.0625rem;
     cursor: pointer;
   }
 
-  div{
-    color: #BAB7D4;
+  div {
+    color: #bab7d4;
     text-align: center;
     font-family: Poppins;
     font-size: 0.6875rem;
@@ -113,15 +135,14 @@ const Box = styled.div`
     line-height: 1.625rem;
     margin-top: 0.5rem;
 
-
-    span{
-      color: #FF7979;
-    font-family: Poppins;
-    font-size: 0.6875rem;
-    font-style: normal;
-    font-weight: 700;
-    line-height: 1.625rem;
-    cursor: pointer;
+    span {
+      color: #ff7979;
+      font-family: Poppins;
+      font-size: 0.6875rem;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 1.625rem;
+      cursor: pointer;
     }
   }
-`
+`;
